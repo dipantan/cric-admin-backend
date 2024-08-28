ca3c1189f3910d2b3e9ee87be6984642e1466ae3
Dipantan Chakraborty <dipantan755@gmail.com>
2024-08-28 20:12:10 +0530

    dsd

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,40 @@
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
-import errorHandler from 'strong-error-handler';
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import errorHandler from "strong-error-handler";
 
-import rootRouter from './routes';
+import rootRouter from "./routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // check if env is set
 if (!process.env.JWT_SECRET) {
-  throw new Error('JWT_SECRET must be defined');
+  throw new Error("JWT_SECRET must be defined");
 }
 
 app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3030',
+    origin: [
+      "http://localhost:3030",
+      "https://cric-admin-backend.onrender.com",
+    ],
   })
 );
 
-app.use('/', rootRouter);
+app.use("/", rootRouter);
 
-app.get('/*', (req, res) => {
+app.get("/*", (req, res) => {
   res.status(404).send({
-    message: 'Not found',
+    message: "Not found",
   });
 });
 
 app.use(
   errorHandler({
-    debug: app.get('env') === 'development',
+    debug: app.get("env") === "development",
     log: true,
   })
 );
